e7559f9b7498a6460856b8cfa1a35c1de2f54cf1
熊书宏 <xiongshuhong@ksjgs.com>
2019-09-17 10:59:53 +0800

    增加uri过滤

--- src/main/java/com/kaishustory/autoconfigure/MicrometerAutoConfiguration.java
+++ src/main/java/com/kaishustory/autoconfigure/MicrometerAutoConfiguration.java
@@ -1,10 +1,13 @@
 package com.kaishustory.autoconfigure;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -17,5 +20,14 @@ public class MicrometerAutoConfiguration {
     public MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name:UNKNOWN}") String applicationName,
                                                              @Value("${spring.profiles.active:UNKNOWN}") String profilesActive) {
         return registry -> registry.config().commonTags("application", applicationName, "profile", profilesActive);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MeterFilter meterFilter() {
+        return MeterFilter.deny(id -> {
+            String uri = id.getTag("uri");
+            return StringUtils.startsWith(uri, "/actuator") || StringUtils.contains(uri, "favicon.ico") || !StringUtils.startsWith(uri, "/");
+        });
     }
 }
